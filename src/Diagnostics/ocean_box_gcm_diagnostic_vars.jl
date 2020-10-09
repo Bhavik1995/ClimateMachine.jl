17c9c26117b1d97186359093322e2dd6672e63a8
K Pamnany <kpamnany@gmail.com>
2020-10-09 15:37:51 -0400

    Split into two modules

--- src/Diagnostics/ocean_box_gcm_diagnostic_vars.jl
+++
@@ -1,5 +0,0 @@
-function setup_diagnostic_variables(
-    ::Type{OceanBoxConfigType},
-    vars::OrderedDict{String, DiagnosticVar},
-)
-end
--- src/Diagnostics/single_stack_diagnostic_vars.jl
+++
@@ -1,5 +0,0 @@
-function setup_diagnostic_variables(
-    ::Type{SingleStackConfigType},
-    vars::OrderedDict{String, DiagnosticVar},
-)
-end
